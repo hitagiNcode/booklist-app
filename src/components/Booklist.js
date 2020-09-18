ca9895d14a621fd86f24abcce622c43f4bd8a8f1
Barış Günenç <power.of.baris@gmail.com>
2020-09-18 15:44:09 +0300

    Setup Comit

---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Booklist extends Component {
+    render() {
+        return (
+            <div className='book-list'>
+                <ul>
+                    <li>the way of kings</li>
+                    <li>sherlok holmes</li>
+                    <li>Kurk mantolu madonna</li>
+                </ul>
+            </div>
+        )
+    }
+}
