c3a24a087833132f0d78d3900c76e38f2f3e8e05
Barış Günenç <power.of.baris@gmail.com>
2020-09-21 02:38:59 +0300

    I started to understand context prodiver

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Booklist from './components/Booklist';
-import Navbar from './components/Navbar';
-import ThemeContextProvider from './contexts/ThemeContext';
+import React from "react";
+import Booklist from "./components/Booklist";
+import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-     <Navbar/>
-     <Booklist/>
-     </ThemeContextProvider>
+        <Navbar />
+        <Booklist />
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 export default class Booklist extends Component {
-    render() {
-        return (
-            <div className='book-list'>
-                <ul>
-                    <li>the way of kings</li>
-                    <li>sherlok holmes</li>
-                    <li>Kurk mantolu madonna</li>
-                </ul>
-            </div>
-        )
-    }
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>the way of kings</li>
+          <li style={{ background: theme.ui }}>sherlok holmes</li>
+          <li style={{ background: theme.ui }}>
+            Kurk mantolu madonna - Sabahhattin Ali
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
