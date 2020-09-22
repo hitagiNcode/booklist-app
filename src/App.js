1fce533dddf59b8bb704db4a25331e4f9df658bc
Barış Günenç <power.of.baris@gmail.com>
2020-09-22 14:17:46 +0300

    Context providers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Booklist from "./components/Booklist";
 import Navbar from "./components/Navbar";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <Booklist />
+        <AuthContextProvider>
+          <Navbar />
+          <Booklist />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
