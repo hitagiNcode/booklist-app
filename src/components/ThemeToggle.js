a6bd6054da74a80f39348a5ed3cb66379287a6fb
Barış Günenç <power.of.baris@gmail.com>
2020-12-24 18:51:28 +0300

    only god knows

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,9 +1,15 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
+const a = 1;
+const b = a == 1 ? 1 :0;
+const xx = 4
+console.log(a*b);
+
 const ThemeToggle = () => {
   const { toggleTheme } = useContext(ThemeContext);
-  return <button onClick={toggleTheme}>Toggle the theme</button>;
+return <button onClick={toggleTheme}>Toggle the theme {a*b*xx.toString()}</button>;
 };
 
 export default ThemeToggle;
+
